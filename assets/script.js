6d32af8c0614b3dd9201244e3d1c3ad7f567ea5d
ChristianAlexander1993 <christian.alexandergit@gmail.com>
2022-08-06 16:24:50 -0700

    new commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var removeItem = document.getElementById("quantity-down");
+removeItem.addEventListener("click", function (event) {
+  console.log("clicked");
+});
+
+var addItem = document.getElementById("quantity-up");
+addItem.addEventListener("click", function (event) {
+  console.log("clicked");
+});
