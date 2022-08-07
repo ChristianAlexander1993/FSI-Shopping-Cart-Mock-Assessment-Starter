6f9d5d296727b9aab333ad39258aae808800da0f
ChristianAlexander1993 <christian.alexandergit@gmail.com>
2022-08-06 21:55:41 -0700

    second commit

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,4 @@
-var removeItem = document.getElementById("quantity-down");
-removeItem.addEventListener("click", function (event) {
-  console.log("clicked");
-});
 
-var addItem = document.getElementById("quantity-up");
-addItem.addEventListener("click", function (event) {
-  console.log("clicked");
-});
+
+var addItem = document.getElementById('quantity-up')
+var remItem = document.getElementById('quantity-down')
